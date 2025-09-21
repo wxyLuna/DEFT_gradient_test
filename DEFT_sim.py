[Debug] fix a mistake of previous_parent/child_vertices_itteration in numerical test

DEFT_sim.py
@@ -1081,19 +1081,20 @@ class DEFT_sim(nn.Module):
                 previous_parent_vertices_iteration_edge1 = b_DLOs_vertices[self.selected_parent_index].clone()
                 previous_parent_vertices_iteration_edge2 = b_DLOs_vertices[self.selected_parent_index].clone()
                 previous_children_vertices_iteration_edge = b_DLOs_vertices[self.selected_children_index].view(self.batch, -1, self.n_vert, 3).clone()
-                # for numerical gradient checking
-                previous_parent_vertices_iteration_edge1_copy = previous_parent_vertices_iteration_edge1.clone()
-                previous_parent_vertices_iteration_edge2_copy = previous_parent_vertices_iteration_edge2.clone()
-                previous_children_vertices_iteration_edge_copy = previous_children_vertices_iteration_edge.clone()
 
                 if inference_1_batch:
                     previous_parent_vertices_iteration_edge1 = previous_parent_vertices_iteration_edge1.detach().cpu().numpy().copy()
                     previous_parent_vertices_iteration_edge2 = previous_parent_vertices_iteration_edge2.detach().cpu().numpy().copy()
                     previous_children_vertices_iteration_edge = previous_children_vertices_iteration_edge.detach().cpu().numpy().copy()
 
-                init_parent_vertices_edge1 = previous_parent_vertices_iteration_edge1_copy
-                init_parent_vertices_edge2 = previous_parent_vertices_iteration_edge2_copy
-                init_children_vertices = previous_children_vertices_iteration_edge_copy
+                init_parent_vertices_edge1 = previous_parent_vertices_iteration_edge1.clone()
+                init_parent_vertices_edge2 = previous_parent_vertices_iteration_edge2.clone()
+                init_children_vertices = previous_children_vertices_iteration_edge.clone()
+
+            # for numerical gradient checking
+            previous_parent_vertices_iteration_edge1_copy = previous_parent_vertices_iteration_edge1.clone()
+            previous_parent_vertices_iteration_edge2_copy = previous_parent_vertices_iteration_edge2.clone()
+            previous_children_vertices_iteration_edge_copy = previous_children_vertices_iteration_edge.clone()
 
             b_DLOs_vertices_input = b_DLOs_vertices.clone() # for numerical gradient checking
             # Reset gradient storage for inextensibility enforcement
@@ -1256,21 +1257,21 @@ class DEFT_sim(nn.Module):
                     self.bkgrad.grad_DX_M = grad_per_ICEC.grad_DX_M
 
                     # Finally, general inextensibility constraints along each branch
-                    # b_DLOs_vertices, grad_per_ICitr = self.constraints_enforcement.Inextensibility_Constraint_Enforcement(
-                    #     self.batch,
-                    #     b_DLOs_vertices,
-                    #     self.batched_m_restEdgeL,
-                    #     self.mass_matrix,
-                    #     self.clamped_index,
-                    #     self.inext_scale,
-                    #     self.mass_scale,
-                    #     self.zero_mask_num,
-                    #     self.b_undeformed_vert,
-                    #     self.bkgrad,
-                    #     self.n_branch
-                    # )
-                    # self.bkgrad.grad_DX_X = grad_per_ICitr.grad_DX_X
-                    # self.bkgrad.grad_DX_M = grad_per_ICitr.grad_DX_M
+                    b_DLOs_vertices, grad_per_ICitr = self.constraints_enforcement.Inextensibility_Constraint_Enforcement(
+                        self.batch,
+                        b_DLOs_vertices,
+                        self.batched_m_restEdgeL,
+                        self.mass_matrix,
+                        self.clamped_index,
+                        self.inext_scale,
+                        self.mass_scale,
+                        self.zero_mask_num,
+                        self.b_undeformed_vert,
+                        self.bkgrad,
+                        self.n_branch
+                    )
+                    self.bkgrad.grad_DX_X = grad_per_ICitr.grad_DX_X
+                    self.bkgrad.grad_DX_M = grad_per_ICitr.grad_DX_M
 
                 ratio, absolute_error, relative_error = self.numerical_gradient_checking(constraint_loop,
                                                                                          self.bkgrad,
@@ -1613,20 +1614,20 @@ class DEFT_sim(nn.Module):
                 DLO_mass[self.selected_children_index],
                 bkgrad
             )
-            # #
-            # positions, grad_per_ICitr = self.constraints_enforcement.Inextensibility_Constraint_Enforcement(
-            #     batch,
-            #     positions,
-            #     nominal_length,
-            #     DLO_mass,
-            #     clamped_index,
-            #     scale,
-            #     perturbed_mass_scale,
-            #     zero_mask_num,
-            #     undeformed_vertices,
-            #     bkgrad,
-            #     n_branch
-            # )
+            #
+            positions, grad_per_ICitr = self.constraints_enforcement.Inextensibility_Constraint_Enforcement(
+                batch,
+                positions,
+                nominal_length,
+                DLO_mass,
+                clamped_index,
+                scale,
+                perturbed_mass_scale,
+                zero_mask_num,
+                undeformed_vertices,
+                bkgrad,
+                n_branch
+            )
 
         return positions
 

constraints_solver.py
@@ -721,16 +721,15 @@ class constraints_enforcement(nn.Module):
             pv_1 = parent_vertices_copy[:, i + 1:i + 2, :].reshape(batch, 3)  # (batch, 3)
             cv_0 = children_vertices_copy[child_idx - 1::2][:, 0, :]# (batch, 3)
             cv_1 = children_vertices_copy[child_idx - 1::2][:, 1, :] # (batch, 3)
-            # pv_init_0 = previous_parent_vertices_copy[:, i:i + 1, :].reshape(batch, 3)  # (batch, 3)
-            # pv_init_1 = previous_parent_vertices_copy[:, i + 1:i + 2, :].reshape(batch, 3)  # (batch, 3)
-            # cv_init_0 = previous_children_vertices_copy[child_idx - 1::2][:, 0, :]
-            # cv_init_1 = previous_children_vertices_copy[child_idx - 1::2][:, 1, :]
-            pv_init_0 = init_parent_vertices_copy[:, i:i + 1, :].reshape(batch, 3)  # (batch, 3)
-            pv_init_1 = init_parent_vertices_copy[:, i + 1:i + 2, :].reshape(batch, 3)  # (batch, 3)
-            cv_init_0 = init_children_vertices_copy[child_idx - 1::2][:, 0, :]
-            cv_init_1 = init_children_vertices_copy[child_idx - 1::2][:, 1, :]
+            pv_init_0 = previous_parent_vertices_copy[:, i:i + 1, :].reshape(batch, 3)  # (batch, 3)
+            pv_init_1 = previous_parent_vertices_copy[:, i + 1:i + 2, :].reshape(batch, 3)  # (batch, 3)
+            cv_init_0 = previous_children_vertices_copy[child_idx - 1::2][:, 0, :]
+            cv_init_1 = previous_children_vertices_copy[child_idx - 1::2][:, 1, :]
+            # pv_init_0 = init_parent_vertices_copy[:, i:i + 1, :].reshape(batch, 3)  # (batch, 3)
+            # pv_init_1 = init_parent_vertices_copy[:, i + 1:i + 2, :].reshape(batch, 3)  # (batch, 3)
+            # cv_init_0 = init_children_vertices_copy[child_idx - 1::2][:, 0, :]
+            # cv_init_1 = init_children_vertices_copy[child_idx - 1::2][:, 1, :]
             epc_0 = pv_init_1 - pv_init_0
-            # print('pv_init_1',pv_init_1)
             ecc_0 = cv_init_1 - cv_init_0
             epc = pv_1 - pv_0
             ecc = cv_1 - cv_0